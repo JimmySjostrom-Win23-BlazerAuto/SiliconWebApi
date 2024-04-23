84f3cdaf4d28efaacabd5555137c79d7db36e57f
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-04-23 11:20:05 +0200

    Updated to allow filtering and searching courses.

--- WebApi/Controllers/CoursesController.cs
+++ WebApi/Controllers/CoursesController.cs
@@ -13,12 +13,18 @@ public class CoursesController(ApiContext context) : ControllerBase
 	private readonly ApiContext _context = context;
 
 	[HttpGet]
-	public async Task<IActionResult> GetAll()
+	public async Task<IActionResult> GetAll(string category = "", string searchQuery = "")
 	{
 		//var courses = await _context.Courses.ToListAsync();
 		//return Ok(courses);
 
 		var query = _context.Courses.Include(i => i.Category).AsQueryable();
+
+		if (!string.IsNullOrWhiteSpace(category) && category != "all")
+				query = query.Where(x => x.Category!.CategoryName == category);	
+
+		if (!string.IsNullOrEmpty(searchQuery))
+			query = query.Where(x => x.Title.Contains(searchQuery) || x.Author.Contains(searchQuery));
 		
 		query = query.OrderByDescending(o => o.LastUpdated);
 
